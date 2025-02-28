434f13ee8d360bc7004c750b27a5d635f74d55f8
Arivy Pereiro <arilope@unc.edum>
2025-02-28 13:20:03 -0500

    Ex02 done

--- exercises/ex02_wordle.py
+++ exercises/ex02_wordle.py
@@ -15,21 +15,27 @@ def contains_char(multiple_letter_word: str, single_character: str) -> bool:
     return False
 
 
-def emojified(
-    guess_word: str,
-    secret_word: str,
-    WHITE_BOX: str = "\U00002B1C",
-    GREEN_BOX: str = "\U0001F7E9",
-    YELLOW_BOX: str = "\U0001F7E8",
-) -> str:
+def emojified(guess_word: str, secret_word: str) -> str:
     """Test characters for colors"""
-    assert len(guess_word) == len(secret_word)
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+    assert len(guess_word) == len(
+        secret_word
+    ), "Guess must be the same length as secret"
     idx: int = 0
-    if guess_word[idx] == secret_word [idx]:
-        return GREEN_BOX
-    elif contains_char == True
-        return YELLOW_BOX
-    return WHITE_BOX
+    full_word: str = ""
+    while idx < len(guess_word):
+        if guess_word[idx] == secret_word[idx]:
+            full_word += GREEN_BOX
+        elif contains_char(
+            multiple_letter_word=secret_word, single_character=guess_word[idx]
+        ):
+            full_word += YELLOW_BOX
+        else:
+            full_word += WHITE_BOX
+        idx += 1
+    return full_word
 
 
 def input_guess(N: int) -> str:
@@ -37,3 +43,24 @@ def input_guess(N: int) -> str:
     while len(guess) != N:
         guess = input(f"That wasn't {N} chars! Try again:")
     return guess
+
+
+def main(secret: str) -> None:
+    """The entrypoint of the program and main game loop."""
+    current_turn: int = 1
+    word_length: int = 5
+    right_guess: bool = False
+    while current_turn < 6:
+        print(f"=== Turn{current_turn}/6 ===")
+        guess_here = input_guess(N=word_length)
+        print(emojified(secret_word=secret, guess_word=guess_here))
+        current_turn += 1
+        if secret == guess_here:
+            print(f"You won in {current_turn} turns!")
+            right_guess = True
+    if right_guess is False:
+        print(f"X/6 - Sorry, try again tomorrow!")
+
+
+if __name__ == "__main__":
+    main(secret="codes")
