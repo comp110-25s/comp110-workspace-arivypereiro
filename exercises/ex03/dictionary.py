f1e99f4bc2bc4c74ff2b08355ac1202c8ef842d4
Arivy Pereiro <arilope@unc.edum>
2025-03-25 15:36:21 -0400

    testing first part

--- exercises/ex03/dictionary.py
+++ exercises/ex03/dictionary.py
@@ -2,4 +2,12 @@
 
 __author__ = "730671996"
 
-my_dictionary: dict[str, str] = {}
+
+def invert(my_dictionary: dict[str, str]):
+    inverted = {}
+    for key in my_dictionary:
+        value = my_dictionary[key]
+        if value in inverted:
+            raise KeyError("error key duplicate")
+        my_dictionary[value] = key
+    return my_dictionary
--- exercises/ex03/dictionary_test.py
+++ exercises/ex03/dictionary_test.py
@@ -1,3 +1,23 @@
 __author__ = "730671996"
 
-from dictionary import my_dictionary
+import pytest
+from exercises.ex03.dictionary import invert
+
+
+def key_error_invert_test() -> None:
+    """Testing if values and keys invert correctly in dictionary."""
+    with pytest.raises(KeyError):
+        new_dictionary = {"x": "y", "w": "z"}
+        invert(new_dictionary)
+
+
+def inverting_correctly_test() -> None:
+    new_dictionary = {"x": "y", "w": "z"}
+    expected_dictionary = {"y": "x", "z": "w"}
+    assert invert(new_dictionary) == expected_dictionary
+
+
+def inverting_nothing_correctly_test() -> None:
+    new_dictionary = {}
+    expected_dictionary = {}
+    assert invert(new_dictionary) == expected_dictionary
