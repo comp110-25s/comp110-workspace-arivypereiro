fe4dc07aff343955dafb10cc0cebfe8c9fa9c80c
Arivy Pereiro <arilope@unc.edum>
2025-03-25 22:19:34 -0400

    ex03 more progress

--- exercises/ex03/dictionary.py
+++ exercises/ex03/dictionary.py
@@ -3,11 +3,41 @@
 __author__ = "730671996"
 
 
-def invert(my_dictionary: dict[str, str]):
-    inverted = {}
+def invert(my_dictionary: dict[str, str]) -> dict[str, str]:
+    inverted: dict[str, str] = {}
     for key in my_dictionary:
         value = my_dictionary[key]
         if value in inverted:
             raise KeyError("error key duplicate")
         my_dictionary[value] = key
     return my_dictionary
+
+
+def count(strs: list[str]) -> dict[str, int]:
+    counts: dict[str, int] = {}
+    for s in strs:
+        if s in counts:
+            counts[s] += 1
+        else:
+            counts[s] = 1
+    return counts
+
+
+def favorite_color(color_dictionary: dict[str, str]) -> str:
+    favorites: dict[str, str] = {}
+    for key in color_dictionary:
+        number_of_favorite = 0
+        if color_dictionary[key] in favorites:
+            number_of_favorite += 1
+            return color_dictionary[key]
+
+def bin_len(bin_list:list[str]) -> dict[int,set[str]]:
+    bins = dict[int,set[str]] = {}
+    for b in bin_list:
+        if len(b) is in bins:
+            bins[len(b)] += 1
+        else:
+            bins[len(b)] = 1
+    return bins
+
+
--- exercises/ex03/dictionary_test.py
+++ exercises/ex03/dictionary_test.py
@@ -1,23 +1,70 @@
 __author__ = "730671996"
 
 import pytest
-from exercises.ex03.dictionary import invert
+from dictionary import invert
 
 
-def key_error_invert_test() -> None:
-    """Testing if values and keys invert correctly in dictionary."""
+def invert_test_error() -> None:
+    """Testing for key error in dictionary."""
     with pytest.raises(KeyError):
-        new_dictionary = {"x": "y", "w": "z"}
-        invert(new_dictionary)
+        invert({"x": "y", "w": "y"})
 
 
-def inverting_correctly_test() -> None:
-    new_dictionary = {"x": "y", "w": "z"}
-    expected_dictionary = {"y": "x", "z": "w"}
-    assert invert(new_dictionary) == expected_dictionary
+def invert_test_two() -> None:
+    """Testing if values and keys invert correctly in dictionary."""
+    assert invert({"x": "y", "w": "z"}) == {"y": "x", "z": "w"}
 
 
-def inverting_nothing_correctly_test() -> None:
-    new_dictionary = {}
-    expected_dictionary = {}
-    assert invert(new_dictionary) == expected_dictionary
+def invert_test_empty() -> None:
+    """Testing if empty dictionary inverts correctly."""
+    assert invert({}) == {}
+
+
+from dictionary import count
+
+
+def test_count_key() -> None:
+    """Testing if key is a unique value"""
+    with pytest.raises(KeyError):
+        count({"a", "a", "bb"})
+
+
+from dictionary import favorite_color
+
+
+def test_same_favorite_color() -> None:
+    """Testing if key is favorite color"""
+    assert favorite_color({"John": "blue", "Joseph": "blue", "Mary": "blue"}) == "blue"
+
+
+def test_frequent_favorite_color() -> None:
+    """Testing if key is favorite color"""
+    assert (
+        favorite_color(
+            {"George": "yellow", "John": "green", "Joseph": "yellow", "Mary": "red"}
+        )
+        == "yellow"
+    )
+
+
+def test_tie_favorite_color() -> None:
+    """Testing if key is favorite color"""
+    assert (
+        favorite_color(
+            {"George": "red", "John": "purple", "Joseph": "purple", "Mary": "red"}
+        )
+        == "purple"
+    )
+
+
+from dictionary import bin_len
+
+
+def test_bins_correctly() -> None:
+    """Testing if it correctly bins"""
+    assert bin_len(["the", "quick", "fox"]) == {3: {"the", "fox"}, 5: {"quick"}}
+
+
+def test_bins_duplicates() -> None:
+    """Testing if it correctly bins duuplicates"""
+    assert bin_len(["the", "the", "fox"]) == {3: {"the", "fox"}}
